e8044d565abb0e0386d3367bc5ae88536b9c6f60
krskaustubh <80772510+krs-kaustubh@users.noreply.github.com>
2026-09-07 16:42:40 +0530

    Renaming & Linker Usuage gcc -o

---
+++ 3-math.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+int multiply(int a, int b);
+
+int main()
+{
+    int a, b;
+    std::cout << "Enter two numbers: ";
+    std::cin >> a >> b;
+    std::cout << "Product: " << multiply(a, b) << std::endl;
+    return 0;
+}
